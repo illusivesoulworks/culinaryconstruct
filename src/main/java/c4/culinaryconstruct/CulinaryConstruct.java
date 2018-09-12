e814fe5fefaf583f54469513cd729e4bb38034a8
TheIllusiveC4 <itwasmec4@gmail.com>
2018-09-12 02:22:34 -0400

    version bump to 1.1.2

--- src/main/java/c4/culinaryconstruct/CulinaryConstruct.java
+++ src/main/java/c4/culinaryconstruct/CulinaryConstruct.java
@@ -28,7 +28,7 @@ public class CulinaryConstruct
 {
     public static final String MODID = "culinaryconstruct";
     public static final String NAME = "Culinary Construct";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     private static final boolean DEBUG = false;
 
