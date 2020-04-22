b6f82fff56138b0a4793df01c919b336473a8a83
TheIllusiveC4 <itwasmec4@gmail.com>
2020-04-21 19:45:32 -0700

    version 1.3.4

--- src/main/java/c4/culinaryconstruct/CulinaryConstruct.java
+++ src/main/java/c4/culinaryconstruct/CulinaryConstruct.java
@@ -30,7 +30,7 @@ public class CulinaryConstruct
 {
     public static final String MODID = "culinaryconstruct";
     public static final String NAME = "Culinary Construct";
-    public static final String VERSION = "1.3.3.2";
+    public static final String VERSION = "1.3.4";
 
     private static final boolean DEBUG = false;
 
